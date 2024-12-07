e1a7fa1ba7260d319786147802064578f0130b63
ALVARO ANDRES DELGADO OVIEDO <hobbie7899@gmail.com>
2024-12-06 23:59:02 -0500

    Cambios realizados de correción funcionando totalmente

--- app/src/main/java/com/example/monitoreoacua/views/MainActivity.java
+++ app/src/main/java/com/example/monitoreoacua/views/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.monitoreoacua.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +17,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+    // Usar un Handler para retrasar la transición a RegisterActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Crear el intent para navegar a RegisterActivity
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent); // Iniciar RegisterActivity
+                finish(); // Opcional: cerrar MainActivity para que no se pueda volver a ella
+            }
+        }, 3000); // Retraso de 2000 milisegundos (2 segundos)
     }
+
 }
--- app/src/main/java/com/example/monitoreoacua/views/RegisterActivity.java
+++ app/src/main/java/com/example/monitoreoacua/views/RegisterActivity.java
@@ -27,7 +27,7 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register2);
+        setContentView(R.layout.activity_register);
 
         etNombre = findViewById(R.id.etNombre);
         etFicha = findViewById(R.id.etFicha);
